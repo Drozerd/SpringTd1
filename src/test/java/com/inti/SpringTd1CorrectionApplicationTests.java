8cee79165317fddf5555fe378c70719c705960f9
Drozerd <debeneyf@gmail.com>
2022-11-02 11:16:54 +0100

    Premier Commit : Debut du projet

---
+++ src/test/java/com/inti/SpringTd1CorrectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTd1CorrectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
